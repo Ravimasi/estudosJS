4f56dd56ae17ddf8e8dfb8925ab68c14fb57649a
Raphael Victor <ravicmasilva@gmail.com>
2020-12-05 00:25:37 -0300

    teste

---
+++ arrayEscolhas/script.js
@@ -0,0 +1,7 @@
+//Criar uma array com minhas escolhas
+const escolhas = ["Azul", "Strogonoff", "Gato"]
+
+for (let i = 0; i < Number(escolhas.length); i++) {
+  console.log(`Minha ${i}° escolha é ${escolhas[i]}`);
+  
+}
